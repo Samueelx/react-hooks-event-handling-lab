d65dcb9fb5b3a006c251f39b1402c9e04bf8ffe9
Samueelx <gsammygitau@gmail.com>
2023-01-15 15:54:51 +0300

    Done with the deliverables.

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,14 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    function handleOnChange() {
+        console.log('Entering password...');
+    }
+
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={handleOnChange} />
+        </div>
     )
 }
 
